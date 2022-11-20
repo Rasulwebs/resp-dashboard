a3f6af86d24ddecf84d5591d1dd2bb49543b07a9
Rasulwebs <boboqulovrasul80@gamil.com>
2022-11-20 20:05:32 +0500

    responsive-dashboard-finished

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,24 +1,38 @@
-import React, { forwardRef  } from "react";
+import React, { forwardRef, useRef } from "react";
 import { NavLink } from "react-router-dom";
-import NavItems from '../navbar/NavItems';
-
+import NavItems from "../navbar/NavItems";
 
 import "./Sidebar.css";
 
-const Sidebar = forwardRef((props , navigation) => {
-  
-    // let navItem = useRef();
+const Sidebar = forwardRef((props, navigation) => {
+  const openMenuBtn = useRef();
 
+  // let navItem = useRef();
+  // navigation.current.classList.add("openMenu")
+  console.log(openMenuBtn);
   return (
     <>
       <div className="min-hight">
-        <div className="navigation" ref={navigation}>
+        <div className="cloeMenuHamburg">
+          <i class="bi bi-list" onClick={()=> openMenuBtn.current.classList.toggle("hamburgMenu")}></i>
+        </div>
+        <div className="navigation" ref={openMenuBtn}>
           <ul className="sidebar-list">
             <li className="sidebar-logo">
-            <NavLink to={'/home'} className='nav-logo'>
-              <i className="fa-solid fa-warehouse"></i>
-              Textile
-            </NavLink>
+              <NavLink to={"/home"} className="nav-logo">
+                <span className="textileLogo">
+                  <i className="fa-solid fa-warehouse"></i>
+                  Textile
+                </span>
+              </NavLink>
+              <div className="openmenu">
+                <i
+                  class="bi bi-list"
+                  onClick={() =>
+                    openMenuBtn.current.classList.toggle("hamburgMenu")
+                  }
+                ></i>
+              </div>
             </li>
             <NavItems
               spanClass="icon"
@@ -34,7 +48,7 @@ const Sidebar = forwardRef((props , navigation) => {
               itemName="Tikish bo'limi"
               link="/home/sewing"
             />
-             <NavItems
+            <NavItems
               spanClass="icon"
               iconName="fa-solid fa-vest-patches"
               spanClassName="title"
@@ -48,7 +62,7 @@ const Sidebar = forwardRef((props , navigation) => {
               itemName="Qadoqlash bo'limi"
               link="/home/packaging"
             />
-             <NavItems
+            <NavItems
               spanClass="icon"
               iconName="bi bi-coin"
               spanClassName="title"
