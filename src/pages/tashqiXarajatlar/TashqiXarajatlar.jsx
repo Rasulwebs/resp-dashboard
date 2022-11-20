ba00d55377bb0ecfe5834fc06a04861044c57948
khayitmuradov <sergeytakhirov@gmail.com>
2022-11-20 13:29:55 +0500

    xarajatlar added

--- src/pages/tashqiXarajatlar/TashqiXarajatlar.jsx
+++ src/pages/tashqiXarajatlar/TashqiXarajatlar.jsx
@@ -1,45 +1,93 @@
-import React, { useState } from "react";
+import React from "react";
 import MainTop from "../../components/mainTop/MainTop";
-import Pagination from "../../components/pagination/Pagination";
-import Table from "../../components/Table/Table";
-import Input from "../../components/Input/Input";
+import "./TashqiXarajatlar.css";
 
 const TashqiXarajatlar = () => {
-  const [searchValue, setSearchValue] = useState("");
   return (
     <div className="w-100 px-4">
       <MainTop panelName={"Tashqi Xarajatlar"} panelRole={"Admin"} />
 
-      <div>
-        <div className="w-100 d-flex justify-content-around flex-wrap gap-4 mb-5">
-          <Input
-            inputType={"text"}
-            inputPlaceholder={"Search"}
-            inputName={"input"}
-            inputClassName={"form-control w-25"}
-            labelName={"userName"}
-            inputValue={searchValue}
-            setInputValue={setSearchValue}
-          />
-          <Input
-            inputType={"text"}
-            inputPlaceholder={"Search"}
-            inputName={"input"}
-            inputClassName={"form-control w-25"}
-            labelName={"userName"}
-            inputValue={searchValue}
-            setInputValue={setSearchValue}
-          />
-        </div>
+      <div className="mt-5">
+       
         <div className="mb-5">
-        <Table />
-        <Table />
-        <Table />
+          <div className="accordion accordion-flush d-flex" id="accordionPanelsStayOpenExample">
+            <div className="accordion-item">
+              <h2 id="flush-headingOne" className="accordion-header">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseOne"
+                  aria-expanded="true"
+                  aria-controls="flush-collapseOne"
+                >
+                  Kunlik chiqim
+                </button>
+              </h2>
+              <div
+                id="flush-collapseOne"
+                className="accordion-collapse collapse show"
+                aria-labelledby="flush-headingOne"
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div className="accordion-body">
+                  Kunlik chiqimlar hisoblanadi !
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="flush-headingTwo">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseTwo"
+                  aria-expanded="true"
+                  aria-controls="flush-collapseTwo"
+                >
+                  Xaftalik chiqim
+                </button>
+              </h2>
+              <div
+                id="flush-collapseTwo"
+                className="accordion-collapse collapse show"
+                aria-labelledby="flush-headingTwo"
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div className="accordion-body">
+                Xaftalik chiqimlar hisoblanadi !
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="flush-headingThree">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseThree"
+                  aria-expanded="true"
+                  aria-controls="flush-collapseThree"
+                >
+                  Oylik chiqim
+                </button>
+              </h2>
+              <div
+                id="flush-collapseThree"
+                className="accordion-collapse collapse show"
+                aria-labelledby="flush-headingThree"
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div className="accordion-body">
+                  Oylik chiqimlar hisoblanadi !
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <Pagination />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TashqiXarajatlar
+export default TashqiXarajatlar;
